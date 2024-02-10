93179934a6e273549afddf037423edeb013ea307
y2k <itwisterlx@gmail.com>
2024-02-10 05:29:35 +0100

    update json formating for tests

--- README.md
+++ README.md
@@ -1,7 +1,3 @@
 # Relax Cat telegram bot
 
-### Доработки
-
-- Рекурсивное JSON форматирование для тестов
-
 #### Powered by https://github.com/y2k/language
--- test/test.clj
+++ test/test.clj
@@ -2,6 +2,15 @@
 (require [vendor.effects :as e]
          [main :as app])
 
+(defn- rec_parse [x]
+  (cond
+    (= null x) x
+    (Array/isArray x) (.map x rec_parse)
+    (= (typeof x) "object") (and x (-> (Object/entries x)
+                                       (.reduce (fn [a x] (assoc a (get x 0) (rec_parse (get x 1)))) {})))
+    (= (typeof x) "string") (if (.startsWith x "{") (rec_parse (JSON/parse x)) x)
+    :else x))
+
 (defn- assert [path]
   (->
    (fs/readFile (.replace path "/input/" "/output/") "utf-8")
@@ -20,9 +29,9 @@
                 (args {:perform test_eff_handler})
                 (if (> log.length 0)
                   (let [x (.pop log)]
-                    (if (= (JSON/stringify [x.key x.data]) (JSON/stringify [name args]))
+                    (if (= (JSON/stringify [x.key x.data]) (JSON/stringify (rec_parse [name args])))
                       (Promise/resolve x.out)
-                      (FIXME "Log: " (.replace path "/input/" "/output/") "\n" (JSON/stringify [x.key x.data]) "\n!=\n" (JSON/stringify [name args]) "\n")))
+                      (FIXME "Log: " (.replace path "/input/" "/output/") "\n" (rec_stringify [x.key x.data]) "\n<>\n" (rec_stringify [name args]) "\n")))
                   (do
                     (.push fx_log {:key name :data args})
                     (Promise/resolve null)))))
@@ -31,7 +40,7 @@
              (e/run_effect {:perform test_eff_handler})
              (.then (fn []
                       (if (= log_json null)
-                        (fs/writeFile (.replace path "/input/" "/output/") (JSON/stringify fx_log null 4))
+                        (fs/writeFile (.replace path "/input/" "/output/") (JSON/stringify (rec_parse fx_log) null 4))
                         (if (= log.length 0) null
                             (FIXME "Log not consumed: " path "\n" (JSON/stringify (.toReversed log) null 2)))))))))))))))
 
