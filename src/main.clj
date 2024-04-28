0825f19bda5ce06dc6c77a870d1cb39db2e39b2e
y2k <itwisterlx@gmail.com>
2024-04-28 15:43:22 +0200

    update deps

--- src/main.clj
+++ src/main.clj
@@ -1,22 +1,23 @@
-(ns _ (:require [vendor.effects :as e]))
+(ns _ (:require ["../vendor/packages/effects/effects.2" :as e]))
 
-(defn- eff_fetch [url props] (e/call :fetch [url props]))
-(defn- eff_db    [db]        (e/call :db    db))
+(def- RELOAD_LIMIT 3)
 
-(def RELOAD_LIMIT 3)
+(defn- state [db] (e/call :db db))
+(defn- broadcast [key fx f]
+  (e/then fx (fn [payload] (e/dispatch key (f payload)))))
 
 (defn- try_handle_button_click_image [chat_id message_id count tag user_id img_json]
-  (eff_fetch "https://api.telegram.org/bot~TG_TOKEN~/editMessageMedia"
-             {:method "POST"
-              :body (JSON/stringify
-                     {:media {:type "video" :media img_json.data.images.original.mp4}
-                      :chat_id chat_id
-                      :message_id message_id
-                      :reply_markup {:inline_keyboard
-                                     [[{:text (if (= RELOAD_LIMIT count) "Delete" (+ "Next [" (- RELOAD_LIMIT count) "]"))
-                                        :callback_data (.stringify JSON {:c (+ count 1) :u user_id :t tag})}
-                                       {:text "Done" :callback_data (.stringify JSON {:c -1 :u user_id :t tag})}]]}})
-              :headers {"content-type" "application/json"}}))
+  (e/fetch "https://api.telegram.org/bot~TG_TOKEN~/editMessageMedia"
+           {:method "POST"
+            :body (JSON.stringify
+                   {:media {:type "video" :media img_json.data.images.original.mp4}
+                    :chat_id chat_id
+                    :message_id message_id
+                    :reply_markup {:inline_keyboard
+                                   [[{:text (if (= RELOAD_LIMIT count) "Delete" (+ "Next [" (- RELOAD_LIMIT count) "]"))
+                                      :callback_data (.stringify JSON {:c (+ count 1) :u user_id :t tag})}
+                                     {:text "Done" :callback_data (.stringify JSON {:c -1 :u user_id :t tag})}]]}})
+            :headers {"content-type" "application/json"}}))
 
 (defn- try_handle_button_click [json]
   (if-let [chat_id json?.callback_query?.message?.chat?.id
@@ -29,25 +30,25 @@
            tag payload.t
            _ (= data_user_id user_id)]
     (if (= count -1)
-      (eff_fetch "https://api.telegram.org/bot~TG_TOKEN~/editMessageReplyMarkup"
-                 {:method "POST"
-                  :body (JSON/stringify {:chat_id chat_id :message_id message_id})
-                  :headers {"content-type" "application/json"}})
+      (e/fetch "https://api.telegram.org/bot~TG_TOKEN~/editMessageReplyMarkup"
+               {:method "POST"
+                :body (JSON.stringify {:chat_id chat_id :message_id message_id})
+                :headers {"content-type" "application/json"}})
       (if (<= count RELOAD_LIMIT)
-        (e/broadcast :try_handle_button_click_image
-                     (eff_fetch (str "https://api.giphy.com/v1/gifs/random?rating=pg&api_key=~GIPHY_TOKEN~&tag=" tag))
-                     (fn [json] [chat_id message_id count tag user_id json]))
-        (eff_fetch "https://api.telegram.org/bot~TG_TOKEN~/deleteMessage"
-                   {:method "POST"
-                    :body (JSON/stringify {:chat_id chat_id :message_id message_id})
-                    :headers {"content-type" "application/json"}})))
+        (broadcast :try_handle_button_click_image
+                   (e/fetch (str "https://api.giphy.com/v1/gifs/random?rating=pg&api_key=~GIPHY_TOKEN~&tag=" tag))
+                   (fn [json] [chat_id message_id count tag user_id json]))
+        (e/fetch "https://api.telegram.org/bot~TG_TOKEN~/deleteMessage"
+                 {:method "POST"
+                  :body (JSON.stringify {:chat_id chat_id :message_id message_id})
+                  :headers {"content-type" "application/json"}})))
     (e/pure null)))
 
 (defn- try_handle_cat_command_send [chat_id user_id tag img_json]
-  (eff_fetch
+  (e/fetch
    "https://api.telegram.org/bot~TG_TOKEN~/sendVideo"
    {:method "POST"
-    :body (JSON/stringify {:video img_json.data.images.original.mp4
+    :body (JSON.stringify {:video img_json.data.images.original.mp4
                            :chat_id chat_id
                            :reply_markup {:inline_keyboard
                                           [[{:text (+ "Next [" RELOAD_LIMIT "]")
@@ -64,9 +65,9 @@
                      (.startsWith text "/dog") "puppy"
                      (.startsWith text "/pig") "pig"
                      :else null)]
-    (e/broadcast :try_handle_cat_command_send
-                 (eff_fetch (str "https://api.giphy.com/v1/gifs/random?rating=pg&api_key=~GIPHY_TOKEN~&tag=" tag) {})
-                 (fn [json] [chat_id user_id tag json]))
+    (broadcast :try_handle_cat_command_send
+               (e/fetch (str "https://api.giphy.com/v1/gifs/random?rating=pg&api_key=~GIPHY_TOKEN~&tag=" tag) {})
+               (fn [json] [chat_id user_id tag json]))
     (e/pure null)))
 
 (defn- delete_message_welcome_message [json]
@@ -74,18 +75,18 @@
            message_id json?.result?.message_id]
     (e/fork
      (e/seq (e/sleep 30)
-            (eff_fetch "https://api.telegram.org/bot~TG_TOKEN~/deleteMessage"
-                       {:method "POST"
-                        :body (JSON/stringify {:chat_id chat_id :message_id message_id})
-                        :headers {"content-type" "application/json"}})))
+            (e/fetch "https://api.telegram.org/bot~TG_TOKEN~/deleteMessage"
+                     {:method "POST"
+                      :body (JSON.stringify {:chat_id chat_id :message_id message_id})
+                      :headers {"content-type" "application/json"}})))
     (e/pure null)))
 
 (defn- try_handle_new_user_end [chat_id message_id user img_json cas_json]
   (if cas_json.ok
-    (eff_fetch
+    (e/fetch
      "https://api.telegram.org/bot~TG_TOKEN~/sendVideo"
      {:method "POST"
-      :body (JSON/stringify
+      :body (JSON.stringify
              {:video img_json.data.images.original.mp4
               :chat_id chat_id
               :parse_mode :MarkdownV2
@@ -101,21 +102,21 @@
                  :id user_id}
            message_id json?.message?.message_id
            chat_id json?.message?.chat?.id]
-    (e/broadcast :try_handle_new_user_end
-                 (e/seq (eff_fetch "https://api.giphy.com/v1/gifs/random?rating=pg&api_key=~GIPHY_TOKEN~&tag=cat" {})
-                        (eff_fetch (str "https://api.cas.chat/check?user_id=" user_id) {}))
-                 (fn [[cat_json cas_json]] [chat_id message_id user cat_json cas_json]))
+    (broadcast :try_handle_new_user_end
+               (e/seq (e/fetch "https://api.giphy.com/v1/gifs/random?rating=pg&api_key=~GIPHY_TOKEN~&tag=cat" {})
+                      (e/fetch (str "https://api.cas.chat/check?user_id=" user_id) {}))
+               (fn [[cat_json cas_json]] [chat_id message_id user cat_json cas_json]))
     (e/pure null)))
 
 (defn- handle_rate_limit [data]
   (if-let [user_id (or data?.update?.message?.from?.id data?.update?.callback_query?.from?.id)
            _ (> (- data.now (or (get data.db user_id) 0)) 1500)]
-    (e/seq (eff_db (assoc data.db user_id data.now))
+    (e/seq (state (assoc data.db user_id data.now))
            (e/dispatch :telegram data.update))
     (e/pure null)))
 
 (defn handle_event [key data]
-  ;; (println (JSON/stringify {:key key :data data} null 2))
+  ;; (println (JSON.stringify {:key key :data data} null 2))
   (case key
     :raw_telegram (handle_rate_limit data)
     :telegram (e/batch [(try_handle_cat_command data)
@@ -139,18 +140,18 @@
                            env
                            e/attach_empty_effect_handler
                            (e/attach_eff :sleep
-                                         (fn [timeout]
+                                         (fn [_ timeout]
                                            (Promise. (fn [resolve] (setTimeout resolve (* 1000 timeout))))))
                            (e/attach_eff :fork
-                                         (fn [fx]
+                                         (fn [world fx]
                                            (.waitUntil ctx (fx world))
-                                           (Promise/resolve null)))
+                                           (Promise.resolve null)))
                            (e/attach_eff :db
-                                         (fn [db]
-                                           (reset GLOBAL_REQUEST_TIMES db)
-                                           (Promise/resolve null)))
+                                         (fn [_ db]
+                                           (reset! GLOBAL_REQUEST_TIMES db)
+                                           (Promise.resolve null)))
                            (e/attach_eff :fetch
-                                         (fn [[url props]]
+                                         (fn [_ {url :url props :props}]
                                            (-> url
                                                (.replaceAll "~TG_TOKEN~" env.TG_TOKEN)
                                                (.replaceAll "~GIPHY_TOKEN~" env.GIPHY_TOKEN)
@@ -158,10 +159,10 @@
                                                (.then (fn [x] (.json x)))
                                                (.then (fn [r] (println "FETCH: " r) r)))))
                            (e/attach_eff :dispatch
-                                         (fn [[key data]]
+                                         (fn [_ [key data]]
                                            (e/run_effect (handle_event key data) world))))]
                 (e/run_effect (handle_event :raw_telegram {:update update
-                                                           :now (Date/now)
+                                                           :now (Date.now)
                                                            :db (deref GLOBAL_REQUEST_TIMES)}) world))))
      (.catch console.error)
      (.then (fn [] (Response. (str "OK - " (Date.)))))))})
--- test/test.clj
+++ test/test.clj
@@ -1,14 +1,14 @@
-(ns _ (:require [vendor.effects :as e]
-                [main :as app]
+(ns _ (:require ["../vendor/packages/effects/effects.2" :as e]
+                ["../src/main" :as app]
                 [js.fs.promises :as fs]))
 
 (defn- rec_parse [x]
   (cond
     (= null x) x
-    (Array/isArray x) (.map x rec_parse)
-    (= (typeof x) "object") (and x (-> (Object/entries x)
-                                       (.reduce (fn [a x] (assoc a (get x 0) (rec_parse (get x 1)))) {})))
-    (= (typeof x) "string") (if (.startsWith x "{") (rec_parse (JSON/parse x)) x)
+    (Array.isArray x) (.map x rec_parse)
+    (= (type x) "object") (and x (-> (Object.entries x)
+                                     (.reduce (fn [a x] (assoc a (get x 0) (rec_parse (get x 1)))) {})))
+    (= (type x) "string") (if (.startsWith x "{") (rec_parse (JSON.parse x)) x)
     :else x))
 
 (defn- assert [path]
@@ -21,30 +21,30 @@
        (fs/readFile path "utf-8")
        (.then
         (fn [event_json]
-          (let [event (JSON/parse event_json)
+          (let [event (JSON.parse event_json)
                 fx_log []
-                log (.reverse (JSON/parse (or log_json "[]")))]
+                log (.reverse (JSON.parse (or log_json "[]")))]
             (defn- test_eff_handler [name args]
               (if (= :fork name)
                 (args {:perform test_eff_handler})
                 (if (> log.length 0)
                   (let [x (.pop log)
-                        expected (JSON/stringify [x.key x.data])
-                        actual (JSON/stringify (rec_parse [name args]))]
+                        expected (JSON.stringify [x.key x.data])
+                        actual (JSON.stringify (rec_parse [name args]))]
                     (if (= expected actual)
-                      (Promise/resolve [name args])
+                      (Promise.resolve [name args])
                       (FIXME "Log: " (.replace path "/input/" "/output/") "\n" expected "\n<>\n" actual "\n")))
                   (do
                     (.push fx_log {:key name :data args})
-                    (Promise/resolve [name args])))))
+                    (Promise.resolve [name args])))))
             (->
              (app/handle_event event.key event.data)
              (e/run_effect {:perform test_eff_handler})
              (.then (fn []
                       (if (= log_json null)
-                        (fs/writeFile (.replace path "/input/" "/output/") (JSON/stringify (rec_parse fx_log) null 4))
+                        (fs/writeFile (.replace path "/input/" "/output/") (JSON.stringify (rec_parse fx_log) null 4))
                         (if (= log.length 0) null
-                            (FIXME "Log not consumed: " path "\n" (JSON/stringify (.toReversed log) null 2)))))))))))))))
+                            (FIXME "Log not consumed: " path "\n" (JSON.stringify (.toReversed log) null 2)))))))))))))))
 
 (let [path "../test/samples/input/"]
   (->
